545e8ed3132e9e738fa90d78d52c29d13b31ba80
Eric West <esw9999@gmail.com>
2013-01-29 21:24:12 -0600

    Adds Create JRubyFX Project Command

--- JRubyFXML.py
+++ JRubyFXML.py
@@ -25,10 +25,180 @@ import sublime
 import sublime_plugin
 import glob
 import os
+import shutil
+import pprint
 from xml.etree import ElementTree
 
 
 current_path = None
+
+class CreateJrubyfxProjectCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.doCommand()
+
+    def doCommand(self):
+        if not self.find_root():
+            self.construct_excluded_pattern()
+            self.build_dir_paths()
+            self.build_file_paths()
+            self.select_dir()
+        else:
+            self.construct_excluded_pattern()
+            self.build_dir_paths()
+            self.build_file_paths()
+            self.move_current_directory_to_top()
+            self.select_dir()
+
+    def find_root(self):
+        folders = self.window.folders()
+        if len(folders) == 0:
+            from os.path import expanduser
+            self.root = unicode(expanduser("~"))
+            self.rel_path_start = len(self.root) + 1
+            return False
+
+        self.root = folders[0]
+        self.rel_path_start = len(self.root) + 1
+        return True
+
+    def construct_excluded_pattern(self):
+        patterns = [pat.replace('|', '\\') for pat in self.get_setting('excluded_dir_patterns')]
+        self.excluded = re.compile('^(?:' + '|'.join(patterns) + ')$')
+
+    def get_setting(self, key):
+        settings = None
+        view = self.window.active_view()
+
+        if view:
+            settings = self.window.active_view().settings()
+
+        if settings and settings.has('JRubyFXML') and key in settings.get('JRubyFXML'):
+            # Get project-specific setting
+            results =settings.get('JRubyFXML')[key]
+        else:
+            # Get user-specific or default setting
+            settings = sublime.load_settings('JRubyFXML.sublime-settings')
+            results = settings.get(key)
+        return results
+
+    def build_dir_paths(self):
+        self.dir_paths = []
+        self.dir_paths = [["../", "Go up one level in the directory structure"]]
+        self.selected_dir = ""
+        for base, dirs, files in os.walk(self.root):
+            dirs_copy = dirs[:]
+            [dirs.remove(dir) for dir in dirs_copy if self.excluded.search(dir)]
+
+            for dir in dirs:
+                dir_path = os.path.join(base, dir)[self.rel_path_start:]
+                self.dir_paths.append(dir_path)
+
+    def build_file_paths(self):
+        self.directory_files = []
+        directory = self.root + "/" + self.selected_dir
+        for base, dirs, files in os.walk(directory):
+            files_copy = files[:]
+
+            # for file in files:
+            #    self.directory_files.append(file)
+
+    def move_current_directory_to_top(self):
+        view = self.window.active_view()
+
+        if view:
+            cur_dir = os.path.dirname(view.file_name())[self.rel_path_start:]
+            for path in self.dir_paths:
+                if path == cur_dir:
+                    i = self.dir_paths.index(path)
+                    self.dir_paths.insert(1, self.dir_paths.pop(i))
+                    break
+
+    def select_dir(self):
+        self.window.show_quick_panel(self.dir_paths, self.dir_selected, sublime.MONOSPACE_FONT)
+
+
+    def dir_selected(self, selected_index):
+        if selected_index != -1:
+            if selected_index == 0:
+                self.up_one_level()
+            else:
+                self.selected_dir = self.dir_paths[selected_index]
+
+            self.build_file_paths()
+
+            # Add aditional menu options
+            self.directory_files.insert(0, ["Browse Directories", "go back to browsing directories"])
+            self.directory_files.insert(1, ["New JRubyFX Project", "new project in the current directory"])
+
+            # Open window to choose desired action
+            self.window.show_quick_panel(self.directory_files, self.file_selected, sublime.MONOSPACE_FONT)
+
+    def file_selected(self, selected_index):
+        if selected_index != -1:
+            if selected_index == 0:
+                self.select_dir()
+            elif selected_index == 1:
+                self.new_dir()
+
+    def up_one_level(self):
+        self.root = os.path.abspath(os.path.join(self.root, os.path.pardir))
+        self.rel_path_start = len(self.root) + 1
+
+        self.build_dir_paths()
+        self.build_file_paths()
+        self.move_current_directory_to_top()
+        self.select_dir()
+
+    def new_dir(self):
+        self.window.show_input_panel("New project name:", '', self.new_dir_action, None, None)
+
+    def new_dir_action(self, dir_name):
+        full_path = os.path.join(self.root, self.selected_dir, dir_name)
+        if os.path.lexists(full_path):
+            sublime.error_message('Directory already exists:\n%s' % full_path)
+            return
+        else:
+            os.mkdir(full_path)
+            os.mkdir(full_path + os.sep + "src")
+            os.mkdir(full_path + os.sep + "dist")
+            os.mkdir(full_path + os.sep + "build")
+            self.create_project_file(dir_name, full_path)
+            self.remove_window_folders()
+            sublime_command_line(['-a', full_path])
+
+    def create_project_file(self, project_name, full_path):
+        file_name = project_name + ".sublime-project"
+        string_full_path = str(full_path)
+        prj_file_contents = ("{\n"
+                            "    \"folders\":\n"
+                            "    [\n"
+                            "        {\n"
+                            "            \"path\": \"%s\"\n"
+                            "        }\n"
+                            "    ]\n"
+                            "}\n" % string_full_path)
+        self.project_file = os.path.join(full_path, file_name)
+        file_ref = open(self.project_file, "w")
+        file_ref.write((prj_file_contents));
+        file_ref.close()
+
+    def remove_window_folders(self):
+        folders_to_remove = self.window.folders()
+        self.window.run_command('remove_folder', {"dirs":folders_to_remove})
+
+# hack to add folders to sidebar (stolen from wbond)
+def get_sublime_path():
+    if sublime.platform() == 'osx':
+        return '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl'
+    elif sublime.platform() == 'linux':
+        return open('/proc/self/cmdline').read().split(chr(0))[0]
+    else:
+        return sys.executable
+
+def sublime_command_line(args):
+    import subprocess
+    args.insert(0, get_sublime_path())
+    return subprocess.Popen(args)
 
 class CreateFxmlTemplateCommand(sublime_plugin.WindowCommand):
     ROOT_DIR_PREFIX = '[root: '
@@ -43,6 +213,15 @@ class CreateFxmlTemplateCommand(sublime_plugin.WindowCommand):
         self.find_templates()
         self.window.show_quick_panel(self.templates, self.template_selected)
 
+    def refresh_folders(self):
+        try:
+            sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 200);
+            sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 600);
+            sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 1300);
+            sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 2300);
+        except:
+            pass
+
     def create_and_open_file(self, path):
         if not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
@@ -55,7 +234,6 @@ class CreateFxmlTemplateCommand(sublime_plugin.WindowCommand):
             'filename': os.path.basename(path),
             'path': os.path.dirname(path)
         }
-
         global current_path
 
         view = self.window.open_file(path)
@@ -63,6 +241,7 @@ class CreateFxmlTemplateCommand(sublime_plugin.WindowCommand):
 
         if not view.is_loading():
             populate_file(view)
+            self.refresh_folders()
 
     def get_content(self, path):
         content = ''
@@ -108,7 +287,7 @@ class CreateFxmlTemplateCommand(sublime_plugin.WindowCommand):
             results = settings.get('FileTemplates')[key]
         else:
             # Get user-specific or default setting
-            settings = sublime.load_settings('FileTemplates.sublime-settings')
+            settings = sublime.load_settings('JRubyFXML.sublime-settings')
             results = settings.get(key)
         return results
 
@@ -230,3 +409,4 @@ class FileTemplatesListener(sublime_plugin.EventListener):
 def populate_file(view):
     global template
     view.run_command("insert_snippet", {'contents': template["content"]})
+    view.window().run_command("refresh_folder_list")
--- README.md
+++ README.md
@@ -19,20 +19,29 @@ Install via [Package Manager](http://wbond.net/sublime_packages/package_control)
 ## How to Use
 FXML's syntax highlighting should just work. To trigger the snippets and completions, simply begin typing the appropriate JRubyFX methods.
 
+### Create a new JRubyFX project
+*This will remove all folders from the current window's project, so open a new window if concerned. Otherwise, continue.*
+
+Press `ctrl-shift-p` to bring up the Command Palette. Select "Create JRubyFX Project." It will open a list of directories either in your current directory, or the user's home directory if the window has no project. Use [Fuzzy Searching](http://docs.sublimetext.info/en/latest/file_management/file_management.html) to select the directory within which you'd like to create the new project. Choose "New JRubyFX Project" and you will be prompted for a name. After naming your project, a new project will be created for you, with an appropriate directory structure already generated. Place all code inside "src". If you need any library code or vendor code, just create a folder for them inside src. Keeping everything in src will make it easy for the other tools to work with.
+
 ### Using fx-generator
 Create a folder with your projects name and cd into it, open sublime text from this directory so that it is your current project. Now create your fxml file, either by hand, or using [SceneBuilder](http://www.oracle.com/technetwork/java/javafx/tools/index.html). Open the completed fxml file in sublime text, and then select Tools/Build System/fx-generator. Now choose Build from the menu, or `ctrl-b`. This should generate a ruby file correctly stubbed out for working with the selected FXML.
 
 For more information on working with JRubyFX and the fx-generator, see [JRubyFX: Getting Started](https://github.com/byteit101/JRubyFXML/blob/master/Getting%20Started.md).
 
+
 ### Generate FXML templates
-  Use `ctrl-shift-p` to open up the Command Palette. Select "Create FXML Template". Next, select the root element for the FXML you would like to generate. If you're unsure, use "AnchorPane". Select the directory you'd like the file placed in(within the current project), and when prompted select a name for the fxml file (.fxml will be appended for you).
+  Use `ctrl-shift-p` to open up the Command Palette. Select "Create FXML Template". Next, select the root element for the FXML you would like to generate. If you're unsure, use "AnchorPane". Select the directory you'd like the file placed in(within the current project), and when prompted select a name for the fxml file (.fxml will be appended for you). If the FXML file doesn't show up in the sidebar immediately after generation, just refresh the sidebar.
 
 ## TODO
 
 - Commands for running and building project
+- Open FXML with SceneBuilder
+- Do something with the awesome GUI REPL, scratchpad.rb.
 - More completions for DSL
 - Syntax highlighting and eventually completions for FX-CSS
 
 
 ## Special Notes
-- The code for the FXML templates came almost unchanged from @mneuhaus ' [SublimeFileTemplate](https://github.com/mneuhaus/SublimeFileTemplates) plugin. I made one minor change to get it working on my system, and cosmetic changes to allow users to target the various FXML root elements without being intermingled with any other .file-template files they might have on their machine.
+- The code for the FXML templates was largely borrowed from @mneuhaus ' [SublimeFileTemplate](https://github.com/mneuhaus/SublimeFileTemplates) plugin. Other plugins which have contributed inspiration to this project are:
+[SideBarEnhancements](https://github.com/titoBouzout/SideBarEnhancements), and [Sublime-File-Navigator](https://github.com/belike81/Sublime-File-Navigator)
